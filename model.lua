89b2042ef7d293e6177568f8b7b84c890b6e4aea
Rafał Korkosz <rkorkosz@milosolutions.com>
2016-06-09 13:34:31 +0200

    add lua hashes resolving

--- model.lua
+++ model.lua
@@ -9,15 +9,33 @@ local function expanding_random(items, weights)
     end
 end
 
-local url = "/container/%s"
+local re = ngx.location.capture(ngx.var.url_ips)
+ngx.var.server = re.body
+
+local url = "/container/%s/tree"
 
 local redis = require "resty.redis"
 local cjson = require "cjson"
 local red = redis:new()
-local ok, err = red:connect("192.168.0.106", 6379)
+local ok, err = red:connect("192.168.88.207", 6379)
 if not ok then
-    ngx.say("Failed to connect to redis", err)
+    ngx.say("Failed to connect to redis: ", err)
     return
 end
 
 local res, _ = red:get(ngx.var.modelId)
+local hashes = cjson.decode(res)
+local items = {}
+local weights = {}
+hash = ""
+if table.getn(hashes) > 1 then
+    for _, v in ipairs(hashes) do
+        table.insert(items, v["hashid"])
+        table.insert(weights, v["percent"])
+    end
+    hash = expanding_random(items, weights)
+else
+    hash = hashes[1]["hashid"]
+end
+ngx.var.path = string.format(url, hash)
+return
